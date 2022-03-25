0de1e55665f2675d437fc810995e7fdffe3d5068
Nick Douglas <douglas.extern@schuettflix.de>
2022-03-25 15:30:22 +0000

    Added register pagae layout & styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { logInUser } from "../lib/api";
 import ErrorMessages from "../components/ErrorMessages.vue";
+import { LockClosedIcon } from "@heroicons/vue/solid";
 
 const email = ref("");
 const password = ref("");
@@ -10,60 +11,60 @@ const submitForm = () => logInUser(email.value, password.value);
 </script>
 
 <template>
-  <div class="flex min-h-full">
-    <div class="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-      <div class="w-full max-w-sm mx-auto lg:w-96">
-        <div>
-          <img class="w-auto h-12" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-          <h2 class="mt-6 text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-        </div>
-        <ErrorMessages />
-        <div class="mt-8">
-          <form method="POST" class="space-y-6" @submit.prevent="submitForm()">
-            <div>
-              <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
-              <div class="mt-1">
-                <input
-                  v-model="email"
-                  id="email"
-                  name="email"
-                  type="email"
-                  autocomplete="email"
-                  required
-                  class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            <div class="space-y-1">
-              <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-              <div class="mt-1">
-                <input
-                  v-model="password"
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="current-password"
-                  required
-                  class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-        </div>
+  <div class="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img class="w-auto h-12 mx-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+        <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">Sign in to your account</h2>
+        <p class="mt-2 text-sm text-center text-gray-600">
+          Or
+          {{ " " }}
+          <router-link to="/register" class="font-medium text-indigo-600 hover:text-indigo-500">create your account here</router-link>
+        </p>
       </div>
-    </div>
-    <div class="relative flex-1 hidden w-0 lg:block">
-      <img class="absolute inset-0 object-cover w-full h-full" src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" alt="" />
+      <ErrorMessages />
+      <form class="mt-8 space-y-6" method="POST" @submit.prevent="submitForm">
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              v-model="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              v-model="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <LockClosedIcon class="w-5 h-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            </span>
+            Sign in
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,81 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { logInUser } from "../lib/api";
+import ErrorMessages from "../components/ErrorMessages.vue";
+
+const email = ref("");
+const name = ref("");
+const password = ref("");
+
+const submitForm = () => {
+  console.log("we got here!");
+};
 </script>
 
 <template>
-  Register
+  <div class="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img class="w-auto h-12 mx-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+        <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">Create your account</h2>
+        <p class="mt-2 text-sm text-center text-gray-600">
+          Or
+          {{ " " }}
+          <router-link to="/" class="font-medium text-indigo-600 hover:text-indigo-500">go back to login</router-link>
+        </p>
+      </div>
+      <ErrorMessages />
+      <form class="mt-8 space-y-6" method="POST" @submit.prevent="submitForm">
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="name" class="sr-only">name</label>
+            <input
+              id="name"
+              v-model="name"
+              name="name"
+              type="text"
+              required
+              class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Your name"
+            />
+          </div>
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              v-model="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              v-model="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Create account
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
